a8925abe404de65b8f5e3c8e1d6d83c9f18d8462
Chad Mills <chad.mills@engageonq.com>
2017-12-24 15:46:57 -0600

    Crazy migration time

--- db/migrate/20160819202954_create_invitationlists.rb
+++ db/migrate/20160819202954_create_invitationlists.rb
@@ -1,4 +1,4 @@
-class CreateMailinglists < ActiveRecord::Migration[5.0]
+class CreateInvitationlists < ActiveRecord::Migration[5.0]
   def change
     create_table :mailing_lists do |t|
       t.string :title, null: false
