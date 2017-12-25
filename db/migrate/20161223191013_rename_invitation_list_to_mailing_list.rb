ccf3923e27d2ad45417354c160d5dbbc39408755
Chad Mills <chad.mills@engageonq.com>
2017-12-24 18:29:46 -0600

    Working on broken migrations

--- db/migrate/20161223191013_rename_invitation_list_to_mailing_list.rb
+++ db/migrate/20161223191013_rename_invitation_list_to_mailing_list.rb
@@ -1,5 +1,5 @@
 class RenameInvitationListToMailingList < ActiveRecord::Migration[5.0]
   def change
-    rename_table :invitation_lists, :mailing_lists
+    #rename_table :invitation_lists, :mailing_lists
   end
 end
